8c6f9a33289fc0858849e4d8f4873559f8f0cd7c
Rob van Eijk <rob@blaeu.com>
2024-08-07 11:35:06 +0200

    added a timeout for unattended operations

--- run_backup.sh
+++ run_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo -e "\nRunning monthly_backup.py with parameter 240501"
-python3 monthly_backup.py --par 240501
+echo -e "\nRunning monthly_backup.py with parameter 240501 and 60 sec timeout for unattended operation"
+python3 monthly_backup.py --par 240806  --timeout
 
-echo -e "\nRunning monthly_backup.py with parameter 240706"
-python3 monthly_backup.py --par 240706
+echo -e "\nRunning monthly_backup.py with parameter 240706 and 60 sec timeout for unattended operation"
+python3 monthly_backup.py --par 240706 --timeout
